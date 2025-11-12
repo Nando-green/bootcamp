82e21a47350a696ab90f72563ad574df4da0d300
Fernando Galarza <nandofergreen27@gmail.com>
2025-11-12 18:32:11 -0500

    new changes script

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-const botones = document.querySelectorAll(".button-apply-job");
-botones.forEach((boton) => {
-    boton.addEventListener("click", function () {
-        boton.textContent = "¡Aplicado!";
-        boton.classList.add("is-applied");
-        boton.disabled = true;
-    });
-});
-
-/*const jobsListingSection = document.querySelector(".jobs-listings");
+const jobsListingSection = document.querySelector(".jobs-listings");
 jobsListingSection.addEventListener("click", function (event) {
     const element = event.target;
     if (element.classList.contains("button-apply-job")) {
@@ -15,4 +6,22 @@ jobsListingSection.addEventListener("click", function (event) {
         element.classList.add("is-applied");
         element.disabled = true;
     }
-});*/
+});
+
+const filter =
+    document.querySelector(
+        "#filter-technology"
+    ); /*recupera el valor de la lista */
+
+filter.addEventListener("change", function () {
+    console.log(filter.value);
+});
+
+const location =
+    document.querySelector(
+        "#filter-location"
+    ); /*recupera el valor de la lista */
+
+location.addEventListener("change", function () {
+    console.log(location.value);
+});
