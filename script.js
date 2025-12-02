56a7d093e38957c74e61ad362dbcb0d511383904
Fernando Galarza <nandofergreen27@gmail.com>
2025-12-02 18:13:23 -0500

    new page

--- script.js
+++ script.js
@@ -28,3 +28,47 @@ const location =
 location.addEventListener("change", function () {
     console.log(location.value);
 });
+
+const searchInput = document.querySelector("#job-search-input");
+searchInput.addEventListener("input", function () {
+    console.log(searchInput.value); /*se verifica el contenido del input*/
+});
+
+searchInput.addEventListener("blur", function () {
+    console.log("se dispara cuando el mouse sale del campo");
+});
+
+const searchForm = document.querySelector("#job-search-form");
+searchForm.addEventListener("submit", function (event) {
+    /*event.preventDefault()*/
+    console.log(
+        "submit"
+    ); /*se verifica el contenido del form atravez del submit*/
+});
+
+document.addEventListener("keydown", function (event) {
+    console.log(
+        "tecla presionada:",
+        event.key
+    ); /*verifica que tecla se presiona*/
+});
+const filterjob = document.querySelector("#filter-location");
+const message = document.querySelector("#filter-select-value");
+const jobs = document.querySelectorAll(".job-listings");
+
+filterjob.addEventListener("change", function () {
+    const selectedValue = filterjob.value;
+    if (selectedValue) {
+        message.textContent = `Has seleccionado:${selectedValue}`;
+    } else {
+        message.textContent = "";
+    }
+    jobs.forEach((job) => {
+        const mode = job.dataset.mode;
+        if (selectedValue === "" || selectedValue === mode) {
+            job.style.display = "flex";
+        } else {
+            job.style.display = "none";
+        }
+    });
+});
